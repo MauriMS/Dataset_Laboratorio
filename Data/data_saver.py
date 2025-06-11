7936df10ba204b8692bdcf977f60a2ce505fabee
Mauri Schefer <95150798+MauriMS@users.noreply.github.com>
2025-06-10 23:53:07 -0300

    Clase de bases de datos agregada

---
+++ Data/data_saver.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+from decouple import config
+
+
+class DataSaver:
+    def __init__(self):
+        user = config('DB_USER')
+        password = config('DB_PASSWORD')
+        host = config('DB_HOST')
+        port = config('DB_PORT')
+        database = config('DB_NAME')
+        
+        #MySQL
+        # url = f'mysql+psycopg2://{user}:{password}@{host}:{port}/{database}'
+        #PostgreSQL
+        url = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
+        self.engine = create_engine(url)
+        
+    def guardaDataFrame(self,df,nombre_tabla):
+        if df is None:
+            print(f'Error no se puede guardar: datos vacios para: {nombre_tabla}')
+            return
+        
+        if not isinstance(df,pd.DataFrame):
+            print(f'Tipo invalido: se esperaba un DataFrame, se recibio {type(df)}')
+            return
+        
+        try:
+            df.to_sql(nombre_tabla,con=self.engine, if_exists='replace', index=False)
+            print(f'Datos guardados en table: {nombre_tabla}')
+            
+            
+        except SQLAlchemyError as e:
+            print(f'Error en guardado de datos: {e}')
