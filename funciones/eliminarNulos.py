522f732112854d5976befdcee741dc9db60164f2
Mauri Schefer <95150798+MauriMS@users.noreply.github.com>
2025-06-11 00:32:55 -0300

    Correciones en la funcion eliminar Nulos

--- funciones/eliminarNulos.py
+++ funciones/eliminarNulos.py
@@ -3,4 +3,4 @@ import pandas as pd
 def eliminarDatosNulos(datos: pd.DataFrame) -> pd.DataFrame:
     if not isinstance(datos,pd.DataFrame):
         raise ValueError('Error el dato debe ser un DataFrame de pandas')
-    return datos.dropna(how='all')
+    return datos.dropna(how='any')
