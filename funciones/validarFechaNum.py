a29678cf83a276794bf18dfe274130156f3b12ff
Mauri Schefer <95150798+MauriMS@users.noreply.github.com>
2025-06-11 14:26:07 -0300

    Archivo Vañodar Fecha y numeros agregado

---
+++ funciones/validarFechaNum.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from funciones.eliminarNulos import eliminarDatosNulos
+
+
+def validarFechaNum(datos:pd.DataFrame) -> pd.DataFrame:
+    for column in datos.columns:
+        if column.endswith('_date'):
+            datos[column] = pd.to_datetime(datos[column], errors='coerce')
+        elif column.endswith('_num'):
+            datos[column] = pd.to_numeric(datos[column], errors='coerce') 
+            
+    if datos.isnull().sum().sum() > 0 :
+            print('Se Detectaron errores de formato en la Fecha y en los numeros\n')
+            print('Se cambiaron datos erroneos por null\n')
+            print('¿Desea eliminar las filas nulas?')
+            Entrada = input('Ingrese SI o NO: ').lower()
+            while Entrada not in ['si', 'no']:
+                Entrada = input('Error por favor ingrese SI o NO: ').lower()
+            if Entrada == 'si':
+                #llamo a la funcion de eliminar datos
+                dt = eliminarDatosNulos(datos)
+                datos = dt
+    
+    return datos
+            
+            
+            
+            
